9d63a0e4c1c3fc6fa810b548e45239c203eb3db4
Anushek Prasal <anushekprasal@gmail.com>
2021-01-18 16:10:30 +0530

    support: Update ColorBlendPreference for Android 11
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- src/com/titanium/support/preferences/ColorBlendPreference.java
+++ src/com/titanium/support/preferences/ColorBlendPreference.java
@@ -324,12 +324,12 @@ public class ColorBlendPreference extends Preference
         public void onClick(View v) {
             if (v == mDialogPreviewColorStart) {
                 ColorPickerDialog pickerDialog =
-                        new ColorPickerDialog(getContext(), mPreviewColorStart);
+                        new ColorPickerDialog(getContext(), mPreviewColorStart, false);
                 pickerDialog.setOnColorChangedListener(mStartColorChangedListener);
                 pickerDialog.show();
             } else if (v == mDialogPreviewColorEnd) {
                 ColorPickerDialog pickerDialog =
-                        new ColorPickerDialog(getContext(), mPreviewColorEnd);
+                        new ColorPickerDialog(getContext(), mPreviewColorEnd, false);
                 pickerDialog.setOnColorChangedListener(mEndColorChangedListener);
                 pickerDialog.show();
             } else if (v == mDialogPreviewColorBetween) {
